2139672629f8bd200cdb27fcae719d657ee3b84e
Anna Trzeciak <trzeciakanna@hotmail.com>
2019-05-17 17:51:46 +0200

    add methods POST, GET ONE, PUT and DELETE book

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -18,4 +18,78 @@ router.get("", (req, res, next) => {
   });
 });
 
+//save new book in db
+router.post("", (req, res, next) => {
+  const book = new Book({
+    title: req.body.book.title,
+    releaseYear: req.body.book.releaseYear,
+    author: req.body.book.author,
+    ISBN: req.body.book.ISBN
+  });
+  book.save().then(item => {
+    console.log("Book added successfully");
+    res.status(201).json({
+      book: {
+        id: item._id,
+        title: item.title,
+        releaseYear: item.releaseYear,
+        author: item.author,
+        ISBN: item.ISBN
+      }
+    });
+  });
+});
+
+//load only one reader (by id)
+router.get("/:id", (req, res, next) => {
+  Book.findById(req.params.id).then(item => {
+    if (item) {
+      console.log("Book found");
+      res.status(200).json({
+        book: {
+          id: item._id,
+          title: item.title,
+          releaseYear: item.releaseYear,
+          author: item.author,
+          ISBN: item.ISBN
+        }
+      });
+    } else {
+      console.log("Book not found");
+      res.status(404);
+    }
+  });
+});
+
+//update book
+router.put("/:id", (req, res, next) => {
+  const book = new Book({
+    _id: req.params.id,
+    title: req.body.book.title,
+    releaseYear:  req.body.book.releaseYear,
+    author: req.body.book.author,
+    ISBN:  req.body.book.ISBN
+  });
+  Book.updateOne({ _id: req.params.id }, book).then(item => {
+    console.log("Book updated");
+    res.status(200).json({
+      book: {
+        id: item._id,
+        title: item.title,
+        releaseYear: item.releaseYear,
+        author: item.author,
+        ISBN: item.ISBN
+      }
+    });
+  });
+});
+
+//delete book
+router.delete("/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id }).then(result => {
+    console.log('Book deleted');
+    res.status(200);
+  });
+});
+
 module.exports = router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,34 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
     fontawesome: {
       icons: {
-        'free-solid-svg-icons': [
-          'caret-down',
-          'user',
-          'ellipsis-v',
-          'plus',
-          'trash-alt',
-          'book-reader',
-          'book'
-         ]
+        "free-solid-svg-icons": [
+          // 'carpet-down',
+          "angle-down",
+          "angle-up",
+          "angle-left",
+          "angle-right",
+          "user",
+          "ellipsis-v",
+          "plus",
+          "trash-alt",
+          "book-reader",
+          "book",
+          "sort-alpha-down",
+          "map-marker-alt",
+          "mobile-alt",
+          "feather-alt",
+          "flag",
+          "calendar-alt"
+        ]
       }
-  }
-});
+    }
+  });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
@@ -33,12 +43,14 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('bower_components/jquery/dist/jquery.min.js');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-  app.import('bower_components/patternfly/dist/js/patternfly.min.js');
-  app.import('bower_components/patternfly/dist/css/patternfly.min.css');
-  app.import('bower_components/patternfly/dist/css/patternfly-additions.min.css');
+  app.import("bower_components/jquery/dist/jquery.min.js");
+  app.import("bower_components/bootstrap/dist/js/bootstrap.min.js");
+  app.import("bower_components/bootstrap/dist/css/bootstrap.min.css");
+  app.import("bower_components/patternfly/dist/js/patternfly.min.js");
+  app.import("bower_components/patternfly/dist/css/patternfly.min.css");
+  app.import(
+    "bower_components/patternfly/dist/css/patternfly-additions.min.css"
+  );
 
   return app.toTree();
 };
